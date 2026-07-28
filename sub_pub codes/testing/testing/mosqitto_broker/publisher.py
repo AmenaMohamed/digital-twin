3a116287cf4904857e80aa846617c63368dcbaa6
Amena Mohamed <amenamohhassan19@gmail.com>
2026-07-28 11:57:49 +0300

    adding json data as a simulation

--- sub_pub codes/testing/testing/mosqitto_broker/publisher.py
+++ sub_pub codes/testing/testing/mosqitto_broker/publisher.py
@@ -1,8 +1,9 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import json
 
 
-broker_address = "broker.hivemq.com" #use external broker
+broker_address = "localhost"#use external broker
 
 import paho.mqtt.client as mqtt
 
@@ -12,12 +13,23 @@ client = mqtt.Client(
 )    #create new instance
 
 client.connect(broker_address) #connect to broker
+#client.publish("ssp/sensors", json_data)
+
+while True:
+    # اقرأ السنسور
+    #temperature = sensor.read_temperature()
+    #humidity = sensor.read_humidity()
+
+#-------------------just for simulation-------------#
+    sensor_data = {
+    "temperature": 25,
+    "humidity": 60,
+    "motion": True
+}
+#-------------------just for simulation-------------#
+
+    client.publish("ssp/sensors",json.dumps(sensor_data))
+    time.sleep(1)
 
 
 
-while True :
-    client.publish("ssp/sensors","OFF")
-    print("Publishing message to topic","ssp/sensors")
-    time.sleep(1) # wait
-
-client.loop_stop() #stop the loop
